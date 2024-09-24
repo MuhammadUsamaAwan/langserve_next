d00f27406af54a4c2f0d203b7572440c072394e7
MuhammadUsamaAwan <underusama@gmail.com>
2024-09-24 13:02:48 +0500

    update

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { RemoteRunnable } from '@langchain/core/runnables/remote';
-import { nanoid } from 'nanoid';
 
 const chain = new RemoteRunnable({
   url: 'http://localhost:8000/llamacpp',
@@ -12,39 +11,38 @@ export default function HomePage() {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([
     {
-      id: nanoid(),
-      content:
-        'You are an expert actor that can fully immerse yourself into any role given. You do not break character for any reason, even if someone tries addressing you as an AI or language model. Currently your role is Satoru Gojo, one of the main protagonists of the Jujutsu Kaisen series. He is a special grade jujutsu sorcerer and widely recognized as the strongest in the world. Satoru is the pride of the Gojo Clan, the first person to inherit both the Limitless and the Six Eyes in four hundred years. He works as a teacher at the Tokyo Jujutsu High and uses his influence to protect and train strong young allies.',
-      type: 'system',
+      id: crypto.randomUUID(),
+      role: 'system',
+      content: 'You are a helpful assistant that can answer questions and help with tasks.',
     },
   ]);
 
   return (
-    <div className='min-h-dvh max-w-xl mx-auto py-20'>
+    <div className='max-w-xl mx-auto py-20'>
       <form
         onSubmit={async e => {
           e.preventDefault();
           const newMessages = [
             ...messages,
             {
-              id: nanoid(),
+              id: crypto.randomUUID(),
+              role: 'user',
               content: input,
-              type: 'human',
             },
           ];
           setMessages(newMessages);
           const stream = chain.streamEvents(newMessages, {
             version: 'v1',
           });
-          const id = nanoid();
+          const id = crypto.randomUUID();
           for await (const chunk of stream) {
             if (chunk.event === 'on_chat_model_start') {
               setMessages(prev => [
                 ...prev,
                 {
                   id,
+                  role: 'assistant',
                   content: '',
-                  type: 'ai',
                 },
               ]);
             } else if (chunk.event === 'on_chat_model_end') {
@@ -68,11 +66,15 @@ export default function HomePage() {
           value={input}
           onChange={e => setInput(e.target.value)}
         />
-        {messages.map((m, i) => (
-          <div key={i}>
-            {m.type === 'human' ? 'User: ' : 'AI: '} {m.content}
-          </div>
-        ))}
+        <div className='space-y-4 mt-4'>
+          {messages
+            .filter(m => m.role !== 'system')
+            .map((m, i) => (
+              <div key={i}>
+                {m.role === 'user' ? 'User: ' : 'AI: '} {m.content}
+              </div>
+            ))}
+        </div>
       </form>
     </div>
   );
